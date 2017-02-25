a7ea895e692992023d0353947b4c49e3bd7d4922
Michael Jancar <mikejancar@outlook.com>
2017-02-25 08:39:10 -0500

    Find the team for a NBA player

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+const express = require('express');
+const nba = require('nba');
+
+const app = express();
+const port = 2016;
+
+app.get('/nba/players/:playername', (request, response) => {
+  let player = nba.findPlayer(request.params.playername);
+  nba.stats.teamInfoCommon({ TeamID: player.teamId })
+    .then((playerTeam) => {
+      let teamInfo = playerTeam.teamInfoCommon[0];
+      let thinPlayer = {
+        'fullName': player.fullName,
+        'team': teamInfo.teamCity + ' ' + teamInfo.teamName
+      };
+      response.send(thinPlayer);
+    });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(`Error occurred: ${err}`);
+  }
+  console.log(`maia-api has started on ${port}...`);
+});
