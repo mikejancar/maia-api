e78053a9568219fb1f2470dc2bce86a05066f2a0
Michael Jancar <mikejancar@outlook.com>
2017-10-21 10:37:13 -0400

    Updated server port to 80

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ const express = require('express');
 const nba = require('nba');
 
 const app = express();
-const port = process.env.PORT || 2016;
+const port = 80;
 
 app.get('/nba/players/:playername', (request, response) => {
-  let player = nba.findPlayer(request.params.playername);
-  nba.stats.teamInfoCommon({ TeamID: player.teamId })
-    .then((playerTeam) => {
-      let teamInfo = playerTeam.teamInfoCommon[0];
-      let thinPlayer = {
-        'fullName': player.fullName,
-        'team': teamInfo.teamCity + ' ' + teamInfo.teamName
-      };
-      response.send(thinPlayer);
-    });
+  const player = nba.findPlayer(request.params.playername);
+  if (player) {
+    nba.stats.teamInfoCommon({ TeamID: player.teamId })
+      .then((playerTeam) => {
+        let teamInfo = playerTeam.teamInfoCommon[0];
+        let thinPlayer = {
+          'fullName': player.fullName,
+          'team': `${teamInfo.teamCity} ${teamInfo.teamName}`
+        };
+        response.send(thinPlayer);
+      })
+      .catch((err) => {
+        response.status(500).send({ message: `Error retrieving player info: ${err}` });
+      });
+  } else {
+    response.status(500).send({ message: `Could not find player ${request.params.playername}` });
+  }
 });
 
 app.listen(port, (err) => {
